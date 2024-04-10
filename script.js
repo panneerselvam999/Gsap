bfa1b6549751882fcf64cb6c2c27a3925d1292d3
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-04-10 15:31:42 -0500

    get started

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.to("h1", {
+    x: 400,
+    duration: 3
+});
+
+
+gsap.to(".star", {
+    x: 500,
+    // scale: 6,
+    rotation: 360,
+    fill: "yellow",
+    duration: 5,
+    stagger: 0.2
+    // speed:9
+})
+
+
+gsap.to(".s-1", {
+    x: 500,
+    // scale: 6,
+    rotationY: 45,
+    fill: "yellow",
+    duration: 5,
+    stagger: 0.2
+    // speed:9
+})
+// gsap.default({ duration: 1 })
+
